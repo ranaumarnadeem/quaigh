5b47ca63608d516efc3f225758635df689be70c5
Mohamed Gaber <me@donn.website>
2026-01-17 03:33:20 +0200

    chore: update to NixOS 25.11
    - update to rustsat{,-kissat} 0.7.4 (saner build system)
      - remove custom rustsat-kissat patch
    - replace cargo2nix (undermaintained) with naersk. bonus: easier to use
    - update CI to use macos-15 for aarch64 Macs, dropped x86_64 Macs
      - should still work but recent changes to NixOS means testing aarch64 is enough + they're getting nixpkgs support removed in 26.11 anyway

--- flake.nix
+++ flake.nix
@@ -1,66 +1,63 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    naersk = {
+      url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-    cargo2nix = {
-      url = "github:cargo2nix/cargo2nix/release-0.12";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.rust-overlay.follows = "rust-overlay";
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    cargo2nix,
-    ...
-  }: {
-    overlays = {
-      default = pkgs': pkgs: {
-        quaigh = pkgs'.rustPkgs.workspace.quaigh {};
-        rustPkgs = pkgs'.rustBuilder.makePackageSet {
-          rustVersion = "1.86.0";
-          # You can regenerate Cargo.nix using this command:
-          #   nix run github:cargo2nix/cargo2nix/v0.12.0
-          packageFun = import ./Cargo.nix;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      naersk,
+      ...
+    }:
+    {
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-          packageOverrides = pkgs:
-            pkgs.rustBuilder.overrides.all
-            ++ [
-              (pkgs.rustBuilder.rustLib.makeOverride {
-                name = "rustsat-kissat";
-                overrideAttrs = {
-                  buildInputs =
-                    [
-                      pkgs.kissat
-                    ]
-                    ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-                      pkgs.openssl
-                    ];
-                  patches = [
-                    ./nix/patches/rustsat-kissat.patch
-                  ];
-                  NIX_KISSAT_DIR = "${pkgs.kissat.lib}";
-                };
-              })
-            ];
+      overlays = {
+        default = pkgs': pkgs: {
+          naersk' = pkgs.callPackage naersk { };
+          quaigh =
+            with pkgs';
+            let
+              libclang = lib.getLib llvmPackages.clang.cc;
+            in
+            naersk'.buildPackage {
+              src = self;
+              env.LIBCLANG_PATH = "${libclang}/lib";
+              buildInputs = [
+                kissat
+                bzip2
+                libclang
+              ];
+            };
         };
       };
-    };
 
-    legacyPackages = nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"] (
-      system:
+      legacyPackages = self.forAllSystems (
+        system:
         import nixpkgs {
           inherit system;
-          overlays = [cargo2nix.overlays.default self.overlays.default];
+          overlays = [
+            naersk.overlays.default
+            self.overlays.default
+          ];
         }
-    );
-    packages = nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"] (system: {
-      inherit (self.legacyPackages."${system}") quaigh;
-      default = self.legacyPackages."${system}".quaigh;
-    });
-  };
+      );
+
+      formatter = self.forAllSystems (system: self.legacyPackages."${system}".nixfmt-tree);
+
+      packages = self.forAllSystems (system: {
+        inherit (self.legacyPackages."${system}") quaigh;
+        default = self.legacyPackages."${system}".quaigh;
+      });
+    };
 }
